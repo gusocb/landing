473a8a6d21f9282297398de616809cc48fe440a2
Gustavo <gustavocruzb@ooutlook.com>
2021-02-02 15:45:59 -0600

    fix:navbar completely responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Header from './components/Header';
-import AboutUs from './components/AboutUs';
-import Home from './components/Home';
-import Services from './components/Services';
-import Contact from './components/Contact';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
 const App = () => {
-	return (
-		<>
-			<Layout>
-				<Header />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/nosotros" component={AboutUs} />
-					<Route exact path="/servicios" component={Services} />
-					<Route exact path="/contacto" component={Contact} />
-				</Switch>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/nosotros" component={AboutUs} />
+        <Route exact path="/servicios" component={Services} />
+        <Route exact path="/contacto" component={Contact} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
-import styled from 'styled-components';
-
-const CoolLink = styled(Link)`
-	text-decoration: none;
-	color: white;
-`;
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
-	return (
-		<Navbar collapseOnSelect expand="lg" variant="dark" bg="primary">
-			<Navbar.Brand>La Azucena</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="ml-auto">
-					<Nav.Link as="li">
-						<CoolLink to="/">Inicio</CoolLink>
-					</Nav.Link>
-					<Nav.Link>
-						<CoolLink to="/nosotros">Nosotros</CoolLink>
-					</Nav.Link>
-					<Nav.Link as="li">
-						<CoolLink to="/servicios">Servicios</CoolLink>
-					</Nav.Link>
-					<Nav.Link as="li">
-						<CoolLink to="/contacto">Contacto</CoolLink>
-					</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	);
+  return (
+    <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
+      <Container>
+        <Navbar.Brand>La Azucena</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link eventKey="1" as={Link} to="/">
+                khe
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="2" as={Link} to="/nosotros">
+                Nosotros
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="3" as={Link} to="/servicios">
+                Servicios
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="4" as={Link} to="/contacto">
+                Contacto
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
