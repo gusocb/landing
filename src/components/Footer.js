035431e080536d0295f93b04c95c8da119439a46
Gustavo <gustavocruzb@ooutlook.com>
2021-02-02 17:49:40 -0600

    fix:footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,15 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import styled from "styled-components";
-
-const FooterContainer = styled.div`
-  background: #273746;
-  margin-bottom: 0;
-  padding: 50px 50px;
-  height: 50px;
-`;
-
-const FooterElement = styled.span`
-  color: white;
-`;
+import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <FooterContainer>
-      <Row>
-        <Col sm className="text-center">
-          <FooterElement>Preguntas Frecuentes</FooterElement>
-        </Col>
-        <Col sm className="text-center">
-          <FooterElement>Reclutamiento</FooterElement>
-        </Col>
-      </Row>
-      <Row>
-        <Col sm className="text-center align-middle">
-          <FooterElement>
-            La Azucena 2021. Todos los derechos reservados.
-          </FooterElement>
-        </Col>
-      </Row>
-    </FooterContainer>
+    <footer class="py-5 bg-dark">
+      <Container>
+        <p class="m-0 text-center text-white">
+          &copy; Todos los derechos reservados. La Azucena 2021.
+        </p>
+      </Container>
+    </footer>
   );
 };
 
