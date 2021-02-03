64c69119732e04496e52e97a0967a0e385acfb44
Gustavo Cruz <gustavocruzb@outlook.com>
2021-02-02 19:18:06 -0600

    fix:banner carousel measures

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from '../assets/banner1.jpeg';
-import banner2 from '../assets/banner2.jpeg';
-import banner3 from '../assets/banner3.jpeg';
+import banner1 from '../assets/banner1.jpg';
+import banner2 from '../assets/banner2.jpg';
+import banner3 from '../assets/banner3.jpg';
 
 const Slides = () => {
 	return (
