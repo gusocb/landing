3bbd4a461236c7625fa772a3c467f6be593706af
Gustavo Cruz <gustavocruzb@outlook.com>
2021-06-07 14:47:03 -0500

    fix: structure change for single page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
-import AboutUs from './pages/AboutUs';
 import Home from './pages/Home';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
 import Footer from './components/Footer';
 
 const App = () => {
 	return (
 		<>
 			<Header />
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/nosotros" component={AboutUs} />
-				<Route exact path="/servicios" component={Services} />
-				<Route exact path="/contacto" component={Contact} />
-			</Switch>
+			<Home />
 			<Footer />
 		</>
 	);
